51af8b0ba056ccb2c429b20a4ac2a255328ee663
Iqbal Ramadhani <iqbal.ramadhani55@gmail.com>
2019-02-01 19:34:50 +0700

    update

--- page.php
+++ page.php
@@ -13,6 +13,7 @@
           'judul'=>'Candi Cangkuang',
           'gambar'=>'https://sejarahlengkap.com/wp-content/uploads/2018/07/Candi-Cangkuang-300x199.jpg',
           'video'=>'https://www.youtube.com/embed/i7E5EKE4KIU',
+          'maps'=>'pb=!1m18!1m12!1m3!1d3959.214951807009!2d107.91746201412703!3d-7.101068094871868!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68b619aaaaaaab%3A0xb5fa65dc245124c3!2sCandi+Cangkuang!5e0!3m2!1sid!2sid!4v1547657527632',
           'isi'=>'<p>Candi Cangkuang adalah sebuah Candi Hindu di Indonesia yang terdapat di Kampung Pulo, wilayah Cangkuang, Kecamatan Leles, Garut, Jawa Barat. Candi Cangkuang juga yang pertama kali ditemukan di Tatar Sunda serta merupakan satu-satunya candi Hindu di Tatar Sunda. Candi ini terletak bersebelahan dengan makam yang sangat bersejarah yaitu Embah Dalem Arief Muhammad, sebuah makam kuno pemuka agama Islam yang dipercaya oleh warga setempat sebagai leluhur penduduk Desa Cangkuang.</p>
           <p>Candi Cangkuang pertama kali ditemukan pada tahun 1966 oleh tim peneliti Harsoyo dan Uka Tjandrasasmita berdasarkan dari laporan Vorderman dalam buku yang dia tulis “Notulen Bataviaasch Genotschap” terbitan tahun 1893 mengenainya adanya sebuah arca yang rusak serta makam kuno di bukit Kampung Pulo, Leles yang tertulis di dalam buku itu. Makam dan arca Syiwa yang dimaksud dalam buku itu memang diketemukan oleh tim peneliti Harsoyo dan Uka Tjandrasasmita.</p>'
         ],
@@ -20,12 +21,14 @@
           'judul'=>'Candi Jiwa',
           'gambar'=>'https://sejarahlengkap.com/wp-content/uploads/2018/07/Candi-Jiwa-300x157.jpg',
           'video'=>'https://www.youtube.com/embed/r2xwXJH8EGI',
+          'maps'=>'pb=!1m18!1m12!1m3!1d3967.544671086184!2d107.15250031412013!3d-6.057019995613591!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a29243e90a1c5%3A0x43c6d6c658acfbdb!2sCandi+Jiwa+Batujaya!5e0!3m2!1sid!2sid!4v1547657622465',
           'isi'=>'<p>Sejarah Candi Jiwa yang ditemukan di kompleks situs Batujaya seperti candi Jiwa, struktur bagian atas dari candi ini menunjukkan bentuk dari sebuah bunga padma (bunga teratai). Pada bagian tengah dari candi ini terdapat sebuah denah yang dibuat dengan struktur melingkar yang sepertinya adalah bekas stupa atau lapik patung Buddha. Pada candi ini tidak ditemukan adanya sebuah tangga, sehingga wujudnya mirip dengan stupa atau arca Buddha di atas dari bunga teratai tersebut. Seperti sebuah teratai yang sedang berbunga mekar dan terapung di atas air. Bentuk seperti yang ada di bangunan candi ini adalah unik dan belum pernah ditemukan di candi yang ada di Indonesia.</p>'
         ],
         [
           'judul'=>'Candi Batujaya',
           'gambar'=>'https://sejarahlengkap.com/wp-content/uploads/2018/07/Percandian-Batujaya-300x154.jpg',
           'video'=>'https://www.youtube.com/embed/Gn5C7lrY6I',
+          'maps'=>'pb=!1m18!1m12!1m3!1d126962.44910351359!2d107.18390242240606!3d-6.0526819968520975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a2bfdca6fa603%3A0x745f5d15db721465!2sBatujaya%2C+Kabupaten+Karawang%2C+Jawa+Barat!5e0!3m2!1sid!2sid!4v1547657764633',
           'isi'=>'<p>Candi di Jawa Barat selaanjutnya adalah kompleks percandian batujaya. Kompleks Percandian Batujaya adalah sebuah kompleks sisa-sisa dari Candi Budha di Indonesia yang terletak di dua kecamatan yaitu Kecamatan Batujaya dan Kecamatan Pakisjaya, Kabupaten Karawang, Provinsi Jawa Barat. Situs ini disebut oleh masyarakat sebagai percandian karena terdiri dari sekumpulan candi yang tersebar di beberapa titik.</p>
           <p>Situs kompleks percandian Batujaya pertama kali diteliti oleh tim arkeologi Fakultas Sastra Universitas Indonesia (sekarang disebuat oleh orang-orang adalah Fakultas Ilmu Budaya UI) pada tahun 1984. Mereka meneliti berdasarkan laporan tentang adanya penemuan benda-benda purbakala oleh warga setempat, di sekitar gundukan-gundukan tanah di tengah-tengah sawah. Gundukan-gundukan ini oleh penduduk setempat disebut sebagai onur atau unur dan dikeramatkan oleh warga sekitar.</p>'
         ],
@@ -33,6 +36,7 @@
           'judul'=>'Candi Cibuaya',
           'gambar'=>'https://sejarahlengkap.com/wp-content/uploads/2018/07/Situs-Cibuaya-300x181.jpg',
           'video'=>'https://www.youtube.com/embed/IKCvq0Hg928',
+          'maps'=>'pb=!1m18!1m12!1m3!1d126967.89004029952!2d107.24331968784185!3d-6.029481028000016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6bd37255fd4107%3A0x401576d14fed930!2sCibuaya%2C+Kabupaten+Karawang%2C+Jawa+Barat!5e0!3m2!1sid!2sid!4v1547658280284',
           'isi'=>'<p>Situs Percandian Cibuaya merupakan sebuah komplek beberapa bangunan dan merupakan peninggalan purbakala di Kecamatan Cibuaya, Kabupaten Karawang, Provinsi Jawa Barat. Berdasarkan dari penemuan arca Wisnu dan lingga, para ahli berpendapat bahwa situs ini merupakan lokasi dari percandian Hindu pada masa lampau.</p>
           <p>Arca Wisnu ditemukan oleh para ahli di Desa Cibuaya pada sekitar tahun 1951 (Wisnu 1) dan 1957 (Wisnu 2), serta tahun 1977 (Wisnu 3) merupakan awal dari ditemukannya Situs Cibuaya oleh para arkeolog. Dengan ditemukannya arca, para arkeolog berpendapat tentang adanya sebuah bangunan suci dan juga sisa pemukiman masyarakat pendukung bangunan suci tersebut di percandian Cibuaya.</p>'
         ],
@@ -40,6 +44,7 @@
           'judul'=>'Candi Karangkamulyan',
           'gambar'=>'https://sejarahlengkap.com/wp-content/uploads/2018/07/Situs-Karangkamulyan-300x159.jpg',
           'video'=>'https://www.youtube.com/embed/rK3aLrGizZk',
+          'maps'=>'pb=!1m18!1m12!1m3!1d63312.9490476749!2d108.45436662848805!3d-7.347236542612051!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f619faaaaaaab%3A0x9d83c25b4d371cb3!2sSitus+Karangkamulyan!5e0!3m2!1sid!2sid!4v1547658379769',
           'isi'=>'<p>Candi di Jawa Barat selanjutnya adalah situs karangkamulyan. Situs Karangkamulyan adalah sebuah situs peninggalan purbakala yang sangat bersejarah dan sebuah situs arkeologi yang terletak di Desa Karangkamulyan, Cijeungjing, Ciamis, Provinsi Jawa Barat, Indonesia. Situs ini merupakan sebuah peninggalan dari zaman kekuasaan Kerajaan Galuh yang bercorak Hindu-Buddha</p>.
           <p>Legenda dari situs Karangkamulyan adalah berkisah tentang Ciung Wanara yang ada hubungannya dengan Kerajaan Galuh. Cerita ini banyak dibumbui dengan sebuah kisah kepahlawanan yang luar biasa dari Ciung Wanara seperti kesaktian dan keperkasaan yang dimilikinya dan tidak dimiliki oleh orang biasa namun hanya dimiliki oleh Ciung Wanara.</p>'
         ],
@@ -47,18 +52,21 @@
           'judul'=>'Candi Gunung Padang',
           'gambar'=>'https://sejarahlengkap.com/wp-content/uploads/2018/07/Situs-Gunung-Padang-300x164.jpg',
           'video'=>'https://www.youtube.com/embed/djfkLlGw_7Q',
+          'maps'=>'pb=!1m18!1m12!1m3!1d3960.1298928720203!2d107.05409701412633!3d-6.993979194947655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6844f87de39b7f%3A0xf33dad8781d10834!2sGunung+Padang+Megalithic+Site!5e0!3m2!1sid!2sid!4v1547658432305',
           'isi'=>'<p>Situs Gunung Padang merupakan sebuah situs prasejarah peninggalan dari kebudayaan Megalitikum di Jawa Barat. Gunung padang tepatnya terletak di perbatasan Dusun Gunungpadang dan Panggulan, Desa Karyamukti, Kecamatan Campaka, Kabupaten Cianjur. Luas kompleks dari Gunung Padang kurang lebih 900 m², terletak pada ketinggian 885 m dpl, dan areal situs ini sekitar 3 ha, menjadikannya sebagai kompleks punden berundak terbesar di Asia Tenggara. Fungsi situs Gunung padang diperkirakan oleh peneliti sebagai tempat pemujaan bagi masyarakat yang bermukim di sekitar Gunungpadang pada sekitar 2000 tahun SM.</p>'
         ],
         [
           'judul'=>'Candi Blandongan',
           'gambar'=>'https://2.bp.blogspot.com/-YXu90h1u5Ko/WMhIkHmg2VI/AAAAAAAAUBI/9_yVloffHosXAsv52EzCpy1TY8svq6Z-QCLcB/s1600/blandongan.jpg',
           'video'=>'https://www.youtube.com/embed/8trHeHb1f2g',
+          'maps'=>'pb=!1m18!1m12!1m3!1d3967.5546776760243!2d107.15132931412008!3d-6.055657995614538!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a29266514df63%3A0xe306fb09e86d55ad!2sCandi+Blandongan!5e0!3m2!1sid!2sid!4v1547658490608',
           'isi'=>'<p>Candi Blandongan adalah sebuah candi yang termasuk ke dalam Situs kompleks percandian Batujaya. Candi ini terletak di Desa Segaran, Kecamatan Batujaya, Kabupaten Karawang, Provinsi Jawa Barat. Beberapa orang yang datang berkunjung atau berwisata ke tempat ini, sering menyebut sebagai situs percandian. Pemugaran dilakukan Candi Blandongan yang terjadi pada tahun 1999/2000 hingga tahun 2010. Di dalam Candi Blandongan ditemukan sebuah amulet dan materai (votive tablet). Amulet adalah salah satu atribut ada dalam  agama Buddha. Amulet biasanya oleh umat Budha digunakan sebagai aktivitas ziarah.</p>'
         ],
         [
           'judul'=>'Candi Tanggulun',
           'gambar'=>'https://sejarahlengkap.com/wp-content/uploads/2018/07/Candi-Tanggulun-300x181.jpg',
           'video'=>'https://www.youtube.com/embed/u9PtTsgP2sc',
+          'maps'=>'pb=!1m18!1m12!1m3!1d7919.213571784833!2d107.75732762161722!3d-7.055404029616702!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68c0d2915b420b%3A0x4ec4300b1fec074d!2sTangulun%2C+Ibun%2C+Bandung%2C+Jawa+Barat!5e0!3m2!1sid!2sid!4v1547658778484',
           'isi'=>'<p>Situs Batu Candi Tanggulun diperkirakan para peneliti merupakan peninggalan agama Hindu karena terdapat sebuah batu lumpang atau yoni. Candi di Jawa Barat yang satu ini terletak di Kampung Talun, Desa Tanggulun, Kecamatan Ibun. Batuan ini berbentuk seperti bujur sangkar dengan ketinggian mencapai 50 cm dan lebar mencapai 40 cm. Pada bagian pinggir-pinggirnya batu candi terdapat sebuah sogatan yang teratur.</p>
           <p>Sekarang candi tersebut berada di sebuah kompleks pemakaman umum. Masyarakat ada yang beranggapan bahwa batu ini merupakan bagian dari kaki sebuah candi. Ada juga yang mengatakan bahwa sebuah batu lumpang atau sebuah yoni. Lainnya lagi menyebutkan bahwa candi ini bagian dari sebuah bangunan sanggah yang berasal dari abad ke-12. Tapi, sebagian orang lagi merasa beranggapan bahwa batu candi ini sebagai tanda batas suatu wilayah pada masau lampau. Secara pasti, memang sampai saat ini belum terungkap.</p>'
         ],
@@ -66,6 +74,7 @@
           'judul'=>'Candi Serut',
           'gambar'=>'https://sejarahlengkap.com/wp-content/uploads/2018/07/Candi-Serut-300x163.jpg',
           'video'=>'https://www.youtube.com/embed/-64A9ntMZW0',
+          'maps'=>'pb=!1m18!1m12!1m3!1d3967.5521365672503!2d107.14870531412002!3d-6.056003895614302!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a2927a9418a29%3A0x343b8b2ce6b68760!2sCandi+Serut+Batujaya+Tarumanegara!5e0!3m2!1sid!2sid!4v1547658826199',
           'isi'=>'<p>Situs Candi Serut merupakan peninggalan agama Budha, yang terletak dikampung gunteng. Pada saat ditemukan kondisi candi ini sangat menggenaskan karena posisi miring dari caandi tersebut, kemiringan ini membuat candi ini diduga roboh oleh para peneliti, bukti robohnya candi oleh para peneliti karena terdapat sebuah tumpukan batu bata disebelahnya.</p>
           <p>Dari kondisi fisik candi tersebut terdapat sebuah lubang yang ditemukan pada dinding candi sebagai bekas balok yang diduga oleh para peneliti sebagai pilar untuk alas dari candi tersebut. Didalam candi ini juga ditemukan sebuah lubang yang berbentuk segi empat ukuran 1 x 1 meter yang kedalamanya belum diketahui oleh para arkeolog, lubang ini sementara diperkirakan oleh para arkeolog sebagai sumur.</p>'
         ],
@@ -73,18 +82,20 @@
           'judul'=>'Candi Sumur (Segaran IX)',
           'gambar'=>'hhttps://sejarahlengkap.com/wp-content/uploads/2018/07/Candi-Sumur-Segaran-IX-300x183.jpg',
           'video'=>'https://www.youtube.com/embed/0JoC3FPQn8I',
-          'isi'=>'<p>Situs Candi Serut merupakan peninggalan agama Budha, yang terletak dikampung gunteng. Pada saat ditemukan kondisi candi ini sangat menggenaskan karena posisi miring dari caandi tersebut, kemiringan ini membuat candi ini diduga roboh oleh para peneliti, bukti robohnya candi oleh para peneliti karena terdapat sebuah tumpukan batu bata disebelahnya.</p>
-          <p>Dari kondisi fisik candi tersebut terdapat sebuah lubang yang ditemukan pada dinding candi sebagai bekas balok yang diduga oleh para peneliti sebagai pilar untuk alas dari candi tersebut. Didalam candi ini juga ditemukan sebuah lubang yang berbentuk segi empat ukuran 1 x 1 meter yang kedalamanya belum diketahui oleh para arkeolog, lubang ini sementara diperkirakan oleh para arkeolog sebagai sumur.</p>'
+          'isi'=>'<p>Situs Candi Sumur merupakan peninggalan bagi umat Buddha, yang terletak ditengah sawah milik warga disebelah barat dari kampung sumur dengan koordinat 107009’ 04’’ BT 06003’34’’ LS. Pada tahun 1992 situs ini pertama kali dilakukan ekskavasi oleh puslit arkenas yang menghasilkan penemuan berupa sebuah bangunan bata yang berbentuk empat persegi panjang yang berukuran 7,35 x 10,55 m, di barat daya dengan ketebalan dindingnya mencapai sekitar 1,70 m, sedangkan di timur laut ketebalan dindingnya mencapai lebih dari 4 m.</p>
+          <p>Situs ini telah dibuatkan atap dan pagar keliling oleh warga setempat agar pengunjung diharapkan bisa nyaman untuk melihat karena letak dari situs ini berada di tengah sawah dan tanah lebih rendah beberapa meter dari permukaan sawah tersebut.</p>'
         ],
         [
           'judul'=>'Candi Tridharma',
           'gambar'=>'https://sejarahlengkap.com/wp-content/uploads/2018/07/Candi-Tridharma-300x137.jpg',
           'video'=>'https://www.youtube.com/embed/SC_NivQZDDE',
+          'maps'=>'pb=!1m18!1m12!1m3!1d3962.119171900098!2d107.01132691412467!3d-6.755318895116799!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69b364bafe3a87%3A0xaa35c8c91052fe43!2sCandi+Tridarma!5e0!3m2!1sid!2sid!4v1547659545144',
           'isi'=>'<p>Candi di Jawa Barat selanjutnya adalah candi tridharma. Pembangunan dari Candi Tridharma dilakukan pada tahun 2000 dan rencananya candi ini akan difungsikan sebagai tempat ibadah bagi umat Budha. Namun, karena berbagai alasan candi ini tidak selasai dibangun sampai tuntas. Saat ini warga setempat mengenalnya sebagai candi yang tak terpakai atau terbengkalai. Kondisi dari candi ini memang terbengkalai, susunan dari candi yang mirip dengan batu ditumbuhi rumput dan lumut. Di sisi lain, justru candi tersebut membuatnya menjadi tampak seperti lebih asli dan seperti dibangun ratusan tahun yang lalu. Candi Tridharma ini sendiri terletak di Kampung Gunung Putri, Kecamatan Pacet, Kabupaten Cianjur, Provinsi Jawa Barat.</p>'
         ],
         [
           'judul'=>'Candi Batu Kalde',
           'gambar'=>'https://cdn.sindonews.net/webp/620/content/2018/12/21/29/1364863/misteri-batu-kalde-pangandaran-tempat-peristirahatan-raja-galuh-Mrh.webp',
+          'maps'=>'pb=!1m18!1m12!1m3!1d3953.7747801428195!2d108.6568255141317!3d-7.707300294444266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6598794ce94a79%3A0x112807845881fa91!2sSitus+Batu+Kalde!5e0!3m2!1sid!2sid!4v1547659654695',
           'video'=>'https://www.youtube.com/embed/tbvbVatN5FI',
           'isi'=>'<p>Batu Kalde adalah sebuah bangunan candi yang terletak di daerah Pananjung, Pangandaran, Provinsi Jawa Barat. Pada sekitaran candi tersebut, ditemukan sebuah balok-balok batu, baik yang masih terkubur di dalam tanah ataupun yang berserakan di permukaan tanah.</p>
           <p>Bangunan Candi di Jawa Barat tersebut jelas menunjukkan bahwa bangunan candi yang dahulu berdiri kokoh di situs tersebut, tentunya peninggalan agama Hindu, bahkan disinyalir bangunan tersebut disebut Candi Pananjung. Dalam sebuah catatan yang dituliskan pendeta Bujangga Manik dalan perjalanan pada abad ke-15 Masehi, sepulangnya dia dari Jawa Tengah dan Timur, disebutkan bahwa pendeta itu singgah di suatu desa yang bernama Pananjung dan desa tersebut terletak di sebuah tanjung yang menjorok ke laut selatan, karena hal itu kenapa desa tersebut dinamakan Pananjung. Tak menutup kemungkinan bahwa jika Bujangga Manik, sang pendeta agama Hindu Sunda tersebut, pernah berkunjung ke Situs Batu Kalde.</p>'
